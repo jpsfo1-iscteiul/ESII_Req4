c6feb6b68fd5bbd8dac6a44e3378e18d3ca30b91
jpsfo1-iscteiul <jpsfo1@iscte-iul.pt>
2020-06-11 22:01:11 +0100

    Updates.

--- ESII/src/main/java/GitHubHandler.java
+++ ESII/src/main/java/GitHubHandler.java
@@ -10,7 +10,9 @@ public class GitHubHandler {
 	public static void main(String[] args) {
 		try {
 			GitHubHandler git = new GitHubHandler();
+			System.out.println("Remote connection to GitHub initiated.\n\n");
 			HTMLHelper helper = new HTMLHelper();
+			System.out.println("Retrieving all specified files. This may take a while.\n\n");
 			helper.fillGitFiles();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -23,15 +25,15 @@ public class GitHubHandler {
 	private GHRepository gitRep;
 	private static String REPO_LINK = "vbasto-iscte/ESII1920";
 	private static String ACCESS_TOKEN = "74ef6e2b3c1cac85df37a9f469d9b8b80e17265d";
-	private List<GHCommit> commitList;
+	private static List<GHCommit> commitList;
 	
 	@SuppressWarnings("static-access")
 	public GitHubHandler() throws IOException {
 		// TODO Auto-generated constructor stub
 		gitBuilder = new GitHubBuilder();
 		git = gitBuilder
-				.withAbuseLimitHandler(AbuseLimitHandler.WAIT)
-				.withRateLimitHandler(RateLimitHandler.WAIT)
+//				.withAbuseLimitHandler(AbuseLimitHandler.WAIT)
+//				.withRateLimitHandler(RateLimitHandler.WAIT)
 				.build()
 				.connectUsingOAuth(ACCESS_TOKEN);
 		gitRep = git.getRepository(REPO_LINK);
@@ -66,11 +68,11 @@ public class GitHubHandler {
 	
 	public String getFileDescription(File file) throws IOException {
 		if(fileListContainsFile(commitList.get(0).getFiles(), file)) {
-			return commitList.get(0).getLastStatus().toString().split("description=")[1].split(",")[0];
+			return commitList.get(0).getCommitShortInfo().getMessage();
 		} else {
 			for (GHTag tag : getTags()) {
 				if(fileListContainsFile(tag.getCommit().getFiles(), file)) {
-					return tag.getCommit().getLastStatus().toString().split("description=")[1].split(",")[0];
+					return tag.getCommit().getCommitShortInfo().getMessage();
 				}
 			}
 		}
--- ESII/src/main/java/HTMLHelper.java
+++ ESII/src/main/java/HTMLHelper.java
@@ -5,44 +5,50 @@ import java.util.List;
 import org.kohsuke.github.GHCommit.File;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GHTag;
+import org.kohsuke.github.PagedIterable;
 
 public class HTMLHelper	{
 	
 	private GitHubHandler git;
 	private GHRepository gitRep;
 	private ArrayList<GitFile> gitFiles;
+	private PagedIterable<GHTag> tags;
+	private static String FILE_NAME = "covid19spreading.rdf";
 	
 	public HTMLHelper() throws IOException {
 		// TODO Auto-generated constructor stub
 		git = new GitHubHandler();
 		gitRep = git.getGitRep();
 		gitFiles = new ArrayList<GitFile>();
+		tags = git.getTags();
 	}
 	
 	public void fillGitFiles() throws IOException {
 		int i = 0;
-		while(i <= git.getTags().toList().size()) {
+		while(i <= tags.toList().size()) {
 			if(i == 0) {
 				List<File> fileList = gitRep.listCommits().toList().get(0).getFiles();
 				for(File file : fileList) {
-					if(file.getFileName().equalsIgnoreCase("covid19spreading.rdf")) {
+					if(file.getFileName().equalsIgnoreCase(FILE_NAME)) {
 						gitFiles.add(new GitFile(git.getFileTimestamp(file), file.getFileName(), git.getFileTag(file), git.getFileDescription(file), git.getSVLink(file)));
+						System.out.println("Successfully retrieved the specified file from master branch.\n");
 					}
 				}
 				i++;
 			} else {
-				for (GHTag tag : git.getTags()) {
+				for (GHTag tag : tags) {
 					List<File> fileList = tag.getCommit().getFiles();
 					for(File file : fileList) {
-						if(file.getFileName().equalsIgnoreCase("covid19spreading.rdf")) {
+						if(file.getFileName().equalsIgnoreCase(FILE_NAME)) {
 							gitFiles.add(new GitFile(git.getFileTimestamp(file), file.getFileName(), git.getFileTag(file), git.getFileDescription(file), git.getSVLink(file)));
+							System.out.println("Successfully retrieved the specified file from tag " + tag.getName() + "\n");
 						}
 					}
 					i++;
 				}
 			}
 		}
-		
+		System.out.println("Done! All files were successfully retried.");
 		for(int test = 0; test < gitFiles.size(); test++) {
 			System.out.println("Timestamp: " + gitFiles.get(test).getTimestamp());
 			System.out.println("Name: " + gitFiles.get(test).getName());
